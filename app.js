79c8320c92286c6faf5ea71c494d2254a26f69e7
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-12 11:18:40 +0200

    Added basic functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+//API Key: &apikey=thewdb
+
+const express = require("express");
+const app = express();
+const request = require("request");
+// app.use("view engine", "ejs");
+
+app.get("/results", function (req, res) {
+    request(
+        "http://www.omdbapi.com/?apikey=thewdb&s=california",
+        (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                let parsedData = JSON.parse(body);
+                res.send(parsedData["Search"][0]["Title"]);
+            }
+        }
+    );
+});
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
