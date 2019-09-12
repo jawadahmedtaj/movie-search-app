5977bbc978647b4afa15d3a2d8463af43bbf44c5
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-12 11:59:35 +0200

    Data displayable on the route

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 const express = require("express");
 const app = express();
 const request = require("request");
-// app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/results", function (req, res) {
     request(
-        "http://www.omdbapi.com/?apikey=thewdb&s=california",
+        "http://www.omdbapi.com/?apikey=thewdb&s=iowa",
         (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 let parsedData = JSON.parse(body);
-                res.send(parsedData["Search"][0]["Title"]);
+                // res.send(parsedData["Search"][0]["Title"]);
+                res.render("results", {
+                    movieData: parsedData
+                });
             }
         }
     );
