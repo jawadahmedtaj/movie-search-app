4423bf592bd92b93be098afca677786790577842
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-12 12:06:33 +0200

    Form added

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = express();
 const request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("search");
+})
+
 app.get("/results", function (req, res) {
+    // console.log(req.query.sTerm)
+    query = req.query.sTerm;
     request(
-        "http://www.omdbapi.com/?apikey=thewdb&s=iowa",
+        "http://www.omdbapi.com/?apikey=thewdb&s=" + query,
         (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 let parsedData = JSON.parse(body);
